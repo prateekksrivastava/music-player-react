aad13785765e28f35c245b86bd77ea3cabacb711
Prateek Srivastava <prateeks@geekyants.com>
2023-04-21 11:29:58 +0530

    initial commit

---
+++ src/components/MusicPlayer.js
@@ -0,0 +1,133 @@
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import siyaram from "../siya-ram.mp3";
+import logo from "../logo.svg";
+
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
+import { IconContext } from "react-icons";
+
+export default function MusicPlayer() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [time, setTime] = useState({
+    min: "",
+    sec: ""
+  });
+  const [currTime, setCurrTime] = useState({
+    min: "",
+    sec: ""
+  });
+
+  const [seconds, setSeconds] = useState();
+
+  const [play, { pause, duration, sound }] = useSound(siyaram);
+
+  useEffect(() => {
+    if (duration) {
+      const sec = duration / 1000;
+      const min = Math.floor(sec / 60);
+      const secRemain = Math.floor(sec % 60);
+      setTime({
+        min: min,
+        sec: secRemain
+      });
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([]));
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        console.log(min);
+        console.log(sec);
+
+        setCurrTime({
+          min,
+          sec
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
+
+  const playingButton = () => {
+    if (isPlaying) {
+      pause();
+      setIsPlaying(false);
+    } else {
+      play();
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <div className="component">
+      <div className="card text-center">
+        <div className="card-header">
+            Playing Now
+        </div>
+        <div className="card-body">
+            <img className="musicCover" src={logo} />
+            <h5 className="card-title">Ram Siya Ram</h5>
+            <p className="card-text text-muted">Sachet Tandon</p>
+            <div>
+                <div className="time">
+                <p>
+                    {currTime.min}:{currTime.sec}
+                </p>
+                <p>
+                    {time.min}:{time.sec}
+                </p>
+                </div>
+                
+                <input
+                type="range"
+                min="0"
+                max={duration / 1000}
+                default="0"
+                value={seconds}
+                className="timeline"
+                onChange={(e) => {
+                    sound.seek([e.target.value]);
+                }}
+                />
+            </div>
+        </div>
+
+        <div>
+            <button className="playButton">
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+                <BiSkipPrevious />
+            </IconContext.Provider>
+            </button>
+            {!isPlaying ? (
+            <button className="playButton" onClick={playingButton}>
+                <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+                <AiFillPlayCircle />
+                </IconContext.Provider>
+            </button>
+            ) : (
+            <button className="playButton" onClick={playingButton}>
+                <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+                <AiFillPauseCircle />
+                </IconContext.Provider>
+            </button>
+            )}
+            <button className="playButton">
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+                <BiSkipNext />
+            </IconContext.Provider>
+            </button>
+        </div>
+
+        <div className="card-footer text-muted">
+            Music Player
+        </div>
+    </div>
+
+      
+    </div>
+  );
+}
